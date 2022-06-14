e128d7114a51403bde9e8997c51e64d0c5d7dc7d
kosukecoulomb <jobch.coulomb@gmail.com>
2022-06-14 08:05:59 +0000

    [update]フォロー機能非同期化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,16 +1,16 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    @user = User.find(params[:user_id])
     following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
-    redirect_to request.referrer || my_page_path
   end
-  
+
   def destroy
+    @user = User.find(params[:user_id])
     following = current_user.relationships.find_by(follower_id: params[:user_id])
     following.destroy
-    redirect_to request.referrer || my_page_path
   end
-  
+
 end
