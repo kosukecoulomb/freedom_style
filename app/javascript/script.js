621ec4aa31b6e098d608c2d881f8c3afac57171e
kosukecoulomb <jobch.coulomb@gmail.com>
2022-06-22 13:12:22 +0000

    [debug]

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,15 +1,15 @@
-// window.addEventListener('turbolinks:load', () => {
-//     const uploader = document.querySelector('.uploader');
-//     uploader.addEventListener('change', (e) => {
-//       const file = uploader.files[0];
-//       const reader = new FileReader();
-//       reader.readAsDataURL(file);
-//       reader.onload = () => {
-//         const coordinate_image = reader.result;
-//         document.querySelector('.avatar').setAttribute('src', coordinate_image);
-//       }
-//     });
-// });
+window.addEventListener('turbolinks:load', () => {
+    const uploader = document.querySelector('.uploader');
+    uploader.addEventListener('change', (e) => {
+      const file = uploader.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const coordinate_image = reader.result;
+        document.querySelector('.avatar').setAttribute('src', coordinate_image);
+      }
+    });
+});
 
 $(document).ready(function () {
   $("#theTarget").skippr({
