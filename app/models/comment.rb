50fb09e2a658785566b5a7009cc2a1b3c169aa7b
kosukecoulomb <jobch.coulomb@gmail.com>
2022-06-29 10:08:24 +0000

    [update]レイアウト調整

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-  
-  #アソシエーション
+
   belongs_to :coordinate
   belongs_to :user
   has_many :notifications, dependent: :destroy
-  
 
+  validates :comment, presence: true, length:{in: 2..250}
 end
