dbd28a2a5cb93fb5f91e666cd56169989e37e676
kosukecoulomb <jobch.coulomb@gmail.com>
2022-07-12 09:41:28 +0000

    [bug]report非同期化

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -5,7 +5,5 @@ class Admin::CommentsController < ApplicationController
     @coordinate = Coordinate.find(params[:coordinate_id])
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to admin_coordinate_path(@coordinate)
-    flash[:notice] = "コメントを削除しました"
   end
 end
--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,13 +2,12 @@ class Admin::ReportsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @reports = Report.all.order(created_at: :desc)
+    @reports = Report.includes(:comment).all.order(created_at: :desc)
   end
 
   def update
     @report = Report.find(params[:id])
     @report.update(checked: true)
-    flash[:notice] = "確認済みに更新しました"
-    redirect_to admin_top_path
+    @reports = Report.includes(:comment).order(created_at: :desc).where(checked: false)
   end
 end
